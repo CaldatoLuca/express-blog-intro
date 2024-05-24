f4f0e44078a2c8ef91a550e23b7f98fd73363545
CaldatoLuca <lucacaldato15@gmail.com>
2024-05-24 15:54:18 +0200

    feat: vista html dei posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,5 @@
 //Utility
-const { readJSON } = require("../utils");
+const { readJSON, readHTML } = require("../utils");
 
 //Posts
 const posts = readJSON("postsDb");
@@ -13,7 +13,19 @@ const home = (req, res) => {
 const index = (req, res) => {
   res.format({
     html: () => {
-      res.send(`<h1>Lista dei posts</h1> <a href='/'>Torna alla home</a>`);
+      //Leggo file HTML
+      const postsHTML = readHTML("posts");
+
+      //creo i li HTML, join lo rende una stringa(quindi leggibile html), altrimenti è un array di tanti `<li>${post.titolo}</li>`
+      const postsListHTML = posts
+        .map((post) => `<li>${post.titolo}</li>`)
+        .join("");
+
+      //replace serve a sostituire il 'segnaposto'(vedi html) con la variabile 'postsListHTML'
+      const postsPageHTML = postsHTML.replace("{{postsList}}", postsListHTML);
+
+      //la risposta è html al 100%
+      res.send(postsPageHTML);
     },
     json: () => {
       res.send({ message: posts });
--- utils.js
+++ utils.js
@@ -1,19 +1,22 @@
 const path = require("path");
 const fs = require("fs");
 
+//Lettura Json
 const readJSON = (fileName) => {
   const filePath = path.join(__dirname, `${fileName}.json`);
   const json = fs.readFileSync(filePath, "utf-8");
   return JSON.parse(json);
 };
 
-const writeJSON = (fileName, data) => {
-  const filePath = path.join(__dirname, `${fileName}.json`);
-  const json = JSON.stringify(data);
-  fs.writeFileSync(filePath, json);
+//Lettura Html
+const readHTML = (fileName) => {
+  return fs.readFileSync(
+    path.join(__dirname, "views", `${fileName}.html`),
+    "utf-8"
+  );
 };
 
 module.exports = {
   readJSON,
-  writeJSON,
+  readHTML,
 };
