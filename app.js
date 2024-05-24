7a99c98634a2f0368512ba71e7d09dd03a94946a
CaldatoLuca <lucacaldato15@gmail.com>
2024-05-24 14:46:23 +0200

    feat: scuffolding

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Benvenuto nel mio Blog</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server pronto a http://localhost:${port}`);
+});
